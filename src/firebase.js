1d3005495985ee59442cb543c226a602a41a5744
Mark Mysler <myslermark@gmail.com>
2023-05-04 11:29:18 -0300

    Variables locales para firebase predeploy

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyAzBvDo7Q4l8_lUNnJT-aUTpjJy4X2gMyU",
-	authDomain: "react-chat-5e8a4.firebaseapp.com",
-	projectId: "react-chat-5e8a4",
-	storageBucket: "react-chat-5e8a4.appspot.com",
-	messagingSenderId: "509701696364",
-	appId: "1:509701696364:web:c2f1f3e4f0463954aae149",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
